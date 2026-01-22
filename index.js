6207fa57d02905dcd55e7687c3eb2cffa77b4ad6
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-22 16:16:06 +0530

    feat: implement Express server with health check endpoint, add database configuration, and define user model

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
-app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
+app.get('/', (req, res) => {
+    return res.json({status: 'Server is up and running...'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+});
